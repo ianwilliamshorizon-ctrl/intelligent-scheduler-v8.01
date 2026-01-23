005e8d62ffdb211428e97b00487b076b9ec021df
ianwilliams.horizon <ianwilliams.horizon@gmail.com>
2026-01-23 21:18:10 +0000

    fix: Correct Vite configuration to expose environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,29 @@
 
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-        input: 'index.html'
-    }
-  },
-}))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+        'process.env': env
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    },
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+          input: 'index.html'
+      }
+    },
+  }
+})
